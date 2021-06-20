56ed153e782f1a8df876b4eb1c8a74aca522003e
jose <josemsb92@gmail.com>
2021-06-20 19:49:01 +0200

    Inicio del proyecto con Arrays Y Caracteres

---
+++ Arrays.java
@@ -0,0 +1,9 @@
+
+public class Arrays {
+	public static void main(String[] args) {
+		int n[]=new int[4];
+		int u[]= {4,58,2,61};
+		n[0]=1;
+		System.out.println(u[1]);
+	}
+}
---
+++ Caracteres.java
@@ -0,0 +1,13 @@
+
+public class Caracteres {
+	//Convierte dos caracteres a String y los concatena para mostrarlos por pantalla
+	public static void main(String[] args) {
+		char caracter = 'C';
+		char caracter2='R';
+		String cadena = String.valueOf(caracter)+String.valueOf(caracter2);
+		
+		System.out.println(cadena);
+		
+	}
+
+}
